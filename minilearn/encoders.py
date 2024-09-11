e9cdfde35cfb3379e1b98087777e39d1e08f51bc
nsandarma <nsandarma@gmail.com>
2024-09-11 08:44:33 +0700

    add encoders : OrdinalEncoder

--- minilearn/encoders.py
+++ minilearn/encoders.py
@@ -11,6 +11,17 @@ class Encoders:
       assert x.ndim != 1 , \
         "Reshape your data either using array.reshape(-1, 1) if your data has a single feature or array.reshape(1, -1) if it contains a single sample."
       self.__categories = [np.unique(x[:,i]) for i in range(x.shape[1])]
+      self.n_features = x.shape[1] # origin
+      c = 0
+      idx_col = []
+      n_features_ = []
+      for i in range(x.shape[1]):
+        n = len(self.categories_[i])
+        idx_col.append(range(c,c+n));
+        c += n
+        n_features_.append(len(self.categories_[i]))
+      self.n_features_ = n_features_ # pre encoding
+      self.idx_cols = idx_col
     else:
       assert x.ndim == 1 , f"should be a 1d array, got an array of shape {x.shape} instead."
       self.categories_ = np.unique(x)
@@ -32,7 +43,7 @@ class Encoders:
       for i in range(x.shape[1]): 
         x_unique = np.unique(x[:,i])
         isNotmember = [j for j in range(len(x_unique)) if x_unique[j] not in self.categories_[i]]
-        assert not isNotmember, f"Found unknown categories {x_unique[isNotmember]} in column {i} during transform "
+        assert not isNotmember, f"Found unknown categories {x_unique[isNotmember]} in column {i} during transform"
     return 
 
   @property
@@ -46,13 +57,11 @@ class OneHotEncoder(Encoders):
   t = "features"
   def __init__(self):
     self.__identity = None
+    self.idx_cols = None
 
   def fit(self,x:np.ndarray):
     super().fit(x)
     self.__identity = [np.eye(len(cat)) for cat in self.categories_]
-    self.__n_features = x.shape[1]
-    self.idx_cols = None
-    return self
 
   def transform(self,x):
     x = np.asarray(x)
@@ -60,17 +69,11 @@ class OneHotEncoder(Encoders):
     self.check_isin(x)
 
     result = np.zeros((x.shape[0], sum(len(cat) for cat in self.categories_)), dtype=int)
-    idx_cols = []
-    current_col = 0
-    self.idx_cols = idx_cols
 
     for i in range(x.shape[1]):
-      idx_col = range(current_col,current_col+len(self.categories_[i]))
-      idx_cols.append(idx_col)
       cat_indices = np.searchsorted(self.categories_[i], x[:, i])
       identity_rows = self.__identity[i][cat_indices]
-      result[:, idx_col] = identity_rows
-      current_col += len(self.categories_[i])
+      result[:, self.idx_cols[i]] = identity_rows
     return result
     
   def fit_transform(self,x):
@@ -82,23 +85,43 @@ class OneHotEncoder(Encoders):
     assert self.idx_cols, "using transform with encoders minilearn !"
     result = np.empty((x.shape[0],self.n_features),dtype=object)
     for i,cat in enumerate(self.categories_):
-      temp = x if len(self.idx_cols) == 1 else x[:,self.idx_cols[i]]
-      res = cat[np.argmax(temp, axis=1)]
+      res = cat[np.argmax(x[:,self.idx_cols[i]], axis=1)]
       result[:, i] = res
     return result
 
   @property
   def identity_(self):
     return self.__identity
-  @property
-  def n_features(self):
-    return self.__n_features
 
 class OrdinalEncoder(Encoders):
   t = "features"
-  def __init__(self):
+  def __init__(self): ...
+
+  def fit(self,x):
     super().fit(x)
+
+  def transform(self,x):
+    x = np.asarray(x)
+    self.check_is_fitted()
+    self.check_isin(x)
+    result  = np.zeros((x.shape[0],x.shape[1]),dtype=int)
+    for i in range(x.shape[1]):
+      cat_indices = np.searchsorted(self.categories_[i],x[:,i])
+      result[:,i] = cat_indices
+    return result
   
+  def fit_transform(self,x):
+    self.fit(x)
+    return self.transform(x)
+  
+  def inverse_transform(self,x):
+    self.check_is_fitted()
+    result = np.empty((x.shape[0],x.shape[1]),dtype=object)
+    for i,cat in enumerate(self.categories_):
+      res = cat[x[:,i]]
+      result[:,i] = res
+    return result
+    
 
 
 class LabelEncoder(Encoders):
@@ -125,19 +148,20 @@ def onehot(x): return OneHotEncoder().fit_transform(x)
 def label(x): return LabelEncoder().fit_transform(x)
 
 if __name__ == "__main__":
-  from sklearn.preprocessing import OneHotEncoder as O
-  x = np.arange(1,10)
-  # x = np.random.randint(1,10,size=(10,))
-  x = np.array(["a","b","c"]).reshape(-1,1)
-  c = np.array(["c","x","c"]).reshape(-1,1)
-  enc = OneHotEncoder()
-  enc.fit(x)
-  print(enc.categories_)
-  print(enc.transform(x))
-  print(enc.inverse_transform(enc.transform(c)))
-  
-  enc = O()
-  enc.fit(x)
-  print(enc.categories_)
-  print(enc.transform(x).toarray())
+  from sklearn import preprocessing
+  import pandas as pd
+  df = pd.read_csv("examples/dataset/drug200.csv")
+  cats = df.select_dtypes("object").values
+  s = cats[np.random.randint(0,len(cats),(10))]
+  enc = OrdinalEncoder()
+  enc.fit(cats)
+  x_transform =  enc.transform(s)
+  print(x_transform)
+  print(enc.inverse_transform(x_transform))
+  print()
     
+  enc = preprocessing.OrdinalEncoder()
+  enc.fit(cats)
+  x_transform =  enc.transform(s)
+  print(x_transform)
+  print(enc.inverse_transform(x_transform))
--- tests/test_encoders.py
+++ tests/test_encoders.py
@@ -1,57 +1,113 @@
 import unittest
-from minilearn.encoders import OneHotEncoder,LabelEncoder
+from minilearn.encoders import OneHotEncoder,LabelEncoder,OrdinalEncoder
 import pandas as pd
 from sklearn import preprocessing
 import numpy as np
 import time
 
+df = pd.read_csv("examples/dataset/drug200.csv")
+cats = df.select_dtypes("object").values
 
 class TestOneHot(unittest.TestCase):
   def setUp(self):
-    df = pd.read_csv("examples/dataset/drug200.csv")
-    cats = df.select_dtypes("object").values
     self.x = cats
-    self.c = cats
+    self.c = cats[:100]
   
   def test_transform(self):
-    # for example 
-    x = np.array(["a","b","c"]).reshape(-1,1)
-    c = np.array(["x","b","c","d"]).reshape(-1,1)
-    
-    start = time.monotonic()
+    start = time.perf_counter()
     enc = OneHotEncoder()
     enc.fit(self.x)
     x_transform1 =  enc.transform(self.c)
-    print(x_transform1)
-    print(f"times (minilearn): {round(time.monotonic() - start,4)}")
+    times = time.perf_counter() - start
 
-    start = time.monotonic()
+    start = time.perf_counter()
     enc = preprocessing.OneHotEncoder()
     enc.fit(self.x)
     x_transform2 = enc.transform(self.c).toarray()
-    print(x_transform2)
-    print(f"times (sklearn) : {round(time.monotonic() - start,4)}")
+    time_threshold = time.perf_counter() - start
 
     np.testing.assert_array_equal(x_transform1,x_transform2)
+    self.assertLess(times,time_threshold)
   
   def test_inverse_transform(self):
-    start = time.monotonic()
+    start = time.perf_counter()
     enc = OneHotEncoder()
     enc.fit(self.x)
     x_transform1 = enc.transform(self.c)
     inverse_transform1 = enc.inverse_transform(x_transform1)
-    print(f"times (minilearn) : {round(time.monotonic() - start,4)}")
+    times = time.perf_counter() - start
 
-    start = time.monotonic()
+    start = time.perf_counter()
     enc = preprocessing.OneHotEncoder()
     enc.fit(self.x)
     x_transform2 = enc.transform(self.c).toarray()
     inverse_transform2 = enc.inverse_transform(x_transform2)
-    print(f"times (sklearn) : {round(time.monotonic() - start,4)}")
+    time_threshold = time.perf_counter() - start
 
     np.testing.assert_array_equal(inverse_transform1,inverse_transform2)
-    
+    self.assertLess(times,time_threshold)
+  
+  def test_checkisin(self):
+    # test pesan error ketika x transform tidak ada di x fit (isNotMember)
+    x = np.array(["a","b","c"]).reshape(-1,1)
+    s = np.array(["a","c","d"]).reshape(-1,1)
+    enc = OneHotEncoder()
+    enc.fit(x)
+    with self.assertRaises(Exception) as context:
+      enc.transform(s)
+    err_msg = "Found unknown categories ['d'] in column 0 during transform"
+    self.assertEqual(str(context.exception),err_msg)
+  
+  def test_n_features(self):
+    # test pesan error ketika n_features pada transform berbeda dengan di fit (shape difference)
+    x = np.array([["a","b","c"],["a","b","c"]])
+    s = np.array(["a","c","d"]).reshape(-1,1)
+    enc = OneHotEncoder()
+    enc.fit(x)
+    with self.assertRaises(Exception) as context:
+      enc.transform(s)
+    err_msg = "X has 1 features, but OneHotEncoder is expecting 3 features as input."
+    self.assertEqual(str(context.exception),err_msg)
+  
 
+class TestOrdinal(unittest.TestCase):
+  def setUp(self):
+    self.x = cats
+    self.c = cats[np.random.randint(0,len(cats),(100,))]
+
+  def test_transform(self):
+    
+    start = time.perf_counter()
+    enc = OrdinalEncoder()
+    enc.fit(self.x)
+    x_transform1 = enc.transform(self.c)
+    times = time.perf_counter() - start
+
+    start = time.perf_counter()
+    enc = preprocessing.OrdinalEncoder()
+    enc.fit(self.x)
+    x_transform2 = enc.transform(self.c)
+    time_threshold = time.perf_counter() - start
+    
+    np.testing.assert_array_equal(x_transform1,x_transform2)
+    self.assertLess(times,time_threshold)
+
+  def test_inverse_transform(self):
+
+    start = time.perf_counter()
+    enc = OrdinalEncoder()
+    enc.fit(self.x)
+    inverse_transform1 = enc.inverse_transform(enc.transform(self.c))
+    times = time.perf_counter() - start
+
+    start = time.perf_counter()
+    enc = preprocessing.OrdinalEncoder()
+    enc.fit(self.x)
+    inverse_transform2 = enc.inverse_transform(enc.transform(self.c))
+    time_threshold = time.perf_counter() - start
+    
+    np.testing.assert_array_equal(inverse_transform1,inverse_transform2)
+    self.assertLess(times,time_threshold)
 
 if __name__ == "__main__":
   unittest.main()
